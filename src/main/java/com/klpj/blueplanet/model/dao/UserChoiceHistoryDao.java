2d302c5468f6f4bce98485cb92f567266bd0ceff
SSAFY-DJ3-JJH <picaqwe104@gmail.com>
2025-04-19 16:11:44 +0900

    DB 전환

--- src/main/java/com/klpj/blueplanet/model/dao/UserChoiceHistoryDao.java
+++ src/main/java/com/klpj/blueplanet/model/dao/UserChoiceHistoryDao.java
@@ -8,4 +8,7 @@ import java.util.List;
 @Repository
 public interface UserChoiceHistoryDao extends JpaRepository<UserChoiceHistory, Long> {
     List<UserChoiceHistory> findByUserStatusIdAndEventId(Long userStatusId, Long eventId);
+
+    // 데이터를 선택 시간 순으로 정렬
+    List<UserChoiceHistory> findByUserStatusIdOrderByChosenAtAsc(Long userStatusId);
 }
