c3dfa3a3c5ea2bfdb5a91665abbb0ef05054ca21
SSAFY-DJ3-JJH <picaqwe104@gmail.com>
2025-04-19 17:03:43 +0900

    error 수정

--- src/main/java/com/klpj/blueplanet/model/services/GptService.java
+++ src/main/java/com/klpj/blueplanet/model/services/GptService.java
@@ -8,11 +8,13 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.Map;
 
+@Service
 public class GptService {
 
     @Value("${openai.api-key:}")
